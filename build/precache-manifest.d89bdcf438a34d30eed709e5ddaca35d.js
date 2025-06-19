338e1f53cab9cf1f7fc985a10d4e4712ce57e444
mo31985 <46001076+mo31985@users.noreply.github.com>
2025-06-20 00:26:11 +0800

    Add files via upload

---
+++ build/precache-manifest.d89bdcf438a34d30eed709e5ddaca35d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444539bcee42a025325dbf43b756868e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9abf402a9250e67298b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "970057458cb8fc6729d7",
+    "url": "/static/js/2.33f0d561.chunk.js"
+  },
+  {
+    "revision": "06540f09072099e4b2d10c9da1e4d3b3",
+    "url": "/static/js/2.33f0d561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e11f79f40ef97165fa4",
+    "url": "/static/js/3.0805f16e.chunk.js"
+  },
+  {
+    "revision": "f9abf402a9250e67298b",
+    "url": "/static/js/main.dbc9c5ef.chunk.js"
+  },
+  {
+    "revision": "960dd12dec17a3ade361",
+    "url": "/static/js/runtime-main.005c5149.js"
+  }
+]);
