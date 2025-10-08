ef26a4463ce1459e7f2f277d30e2798a72dbd1a0
mo31985 <46001076+mo31985@users.noreply.github.com>
2025-10-08 14:42:50 +0800

    Update state.js

--- src/context/state.js
+++ src/context/state.js
@@ -1,72 +1,63 @@
+// src/context/state.js
+
 import React, { createContext, useReducer } from 'react';
+import { DEFAULT_SAVE_LIMIT, DEFAULT_LOAD_LIMIT } from '../constants';
 
 const initialState = {
-  showWelcomeScreen: true,
-  showCharacterCreation: false,
-  showAuthModal: false,
-  authMode: 'login',
-  userEmail: '',
-  userPassword: '',
-  authError: '',
-  authMessage: '',
-  currentChapter: null,
-  choices: [],
-  isLoadingText: false,
-  showChoices: false,
-  showProceedToChoicesButton: false,
-  loading: false,
-  error: '',
-  saveMessage: '',
-  loadMessage: '',
-  isSaving: false,
-  isLoadingSave: false,
+  // ... (你原有的其他状态)
   userId: null,
-  currentUser: null,
-  userProfile: { isVIP: false },
-  characterStats: { strength: 5, intelligence: 5, agility: 5 },
-  allocatedPoints: { strength: 0, intelligence: 0, agility: 0 },
-  statPoints: 10,
-  timeLeft: 0,
-  timerActive: false,
-  userPathHistory: [],
-  isPathHistoryVisible: false,
-  showRestartConfirm: false,
-  feedbackText: '',
-  feedbackMessage: '',
-  isAuthReady: false,
-  initialDataLoaded: false
+  loading: true,
+  error: null,
+  showWelcomeScreen: true,
+  characterStats: { strength: 0, intelligence: 0, agility: 0 },
+  currentChapter: null,
+  
+  // 【新增】追踪存档次数的 state
+  saveCount: 0,
+  loadCount: 0,
+  maxSaveLimit: DEFAULT_SAVE_LIMIT,
+  maxLoadLimit: DEFAULT_LOAD_LIMIT,
 };
-
-function stateReducer(state, action) {
-  switch (action.type) {
-    case 'SET_LOADING':
-      return { ...state, loading: action.payload };
-    case 'SET_CURRENT_CHAPTER':
-      return { ...state, currentChapter: action.payload };
-    case 'SET_SHOW_WELCOME_SCREEN':
-      return { ...state, showWelcomeScreen: action.payload };
-    case 'SET_ERROR':
-      return { ...state, error: action.payload };
-    case 'SHOW_AUTH_MODAL':
-      return { ...state, showAuthModal: true };
-    case 'HIDE_AUTH_MODAL':
-      return { ...state, showAuthModal: false };
-    case 'SET_USER_ID':
-      return { ...state, userId: action.payload };
-    case 'SET_IS_LOADING_TEXT':
-      return { ...state, isLoadingText: action.payload };
-    case 'SET_SHOW_CHOICES':
-      return { ...state, showChoices: action.payload };
-    default:
-      return state;
-  }
-}
 
 export const StateContext = createContext();
 
-export const StateProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(stateReducer, initialState);
+export const reducer = (state, action) => {
+  switch (action.type) {
+    // ... (你原有的其他 case)
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload };
+    case 'SET_ERROR':
+      return { ...state, error: action.payload, loading: false };
+    case 'SET_USER_ID':
+      return { ...state, userId: action.payload };
+    case 'SET_SHOW_WELCOME_SCREEN':
+        return { ...state, showWelcomeScreen: action.payload };
+    case 'SET_CURRENT_CHAPTER':
+        return { ...state, currentChapter: action.payload };
+    
+    // 【重要更新】载入存档时，一并更新存档次数
+    case 'LOAD_USER_DATA':
+      return {
+        ...state,
+        characterStats: action.payload.characterStats,
+        saveCount: action.payload.saveCount || 0,
+        loadCount: action.payload.loadCount || 0,
+      };
 
+    case 'SET_CHARACTER_STATS':
+      return { ...state, characterStats: action.payload };
+
+    // 【新增】更新存档次数的 case
+    case 'INCREMENT_SAVE_COUNT':
+      return { ...state, saveCount: state.saveCount + 1 };
+    
+    default:
+      return state;
+  }
+};
+
+export const StateProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <StateContext.Provider value={{ state, dispatch }}>
       {children}
